b3f7a47e05a27ec5f3632c9a9b4a5ebf54525236
kuan <likuan@node22.(none)>
2018-01-09 11:37:05 +0800

    main

--- part_kaas.py
+++ part_kaas.py
@@ -1,1 +1,15 @@
-/data/home/likuan/bin/part_kaas.py
+#!/usr/bin/env python
+import sys
+from kaas import CurlWeb
+if len(sys.argv)!=5:
+	print "part_kaas.py <jobid> <email> <prefix> <key>"
+	sys.exit()
+jobid=sys.argv[1]
+email=sys.argv[2]
+prefix=sys.argv[3]
+key=sys.argv[4]
+p=CurlWeb(key,Mail=email,jobid=jobid,ResultFileName=prefix)
+p.GetKaasResults()
+p.FormatResults()
+p.GetPathWayImage()
+p.StatisticsLevelB(prefix+"_KEGG_Orthology.keg")
